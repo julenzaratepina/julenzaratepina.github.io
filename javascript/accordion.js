1346ad6efcabcdfb9e0e6d83e66741416cda4ef9
julenzaratepina <74966276+julenzaratepina@users.noreply.github.com>
2020-11-25 18:33:50 +0100

    Update accordion.js

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,7 +1,5 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-var arrowdown = document.getElementbyId("arrowdown");
-var arrowup = document.getElementbyId("arrowup");
 
 
 for (i = 0; i < acc.length; i++) {
