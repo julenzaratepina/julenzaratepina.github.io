28832889a1d01018cc145da23f22f28db5da5971
julenzaratepina <74966276+julenzaratepina@users.noreply.github.com>
2021-11-22 11:13:39 +0000

    Update accordion.js

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -24,7 +24,7 @@ for (i = 0; i < acc.length; i++) {
       
     } else {
       panel.style.display = "block";
-      if this.id === "identity_abstract"{
+      if this.id === "resolution_abstract"{
         document.getElementById("resolution_show").style.display = "none";
         document.getElementById("resolution_hide").style.display = "block";
       } else{
